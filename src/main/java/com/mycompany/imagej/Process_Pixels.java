52f6d8e3f4503b5b4d1511b5acc3285488370491
Gerard Montarou <montarou@clermont.in2p3.fr>
2022-12-15 13:05:25 +0100

    Commit du 15/12

--- src/main/java/com/mycompany/imagej/Process_Pixels.java
+++ src/main/java/com/mycompany/imagej/Process_Pixels.java
@@ -8,6 +8,7 @@
  *     Modification GM 16/11/2022
  */
 
+
 package com.mycompany.imagej;
 
 import ij.IJ;
@@ -74,17 +75,31 @@ public class Process_Pixels implements PlugIn {
 		//  - selection d'une image input
 		//  - selection d'un directory output
 		
-        // JFileChooser(String directoryPath) utilise le chemin donne
+		// Utilisation de la classe JFileChooser (extends JComponent)
+		// JFileChooser fournit un mécanisme simple permettant à l'utilisateur de choisir un fichier
+        // C'est un selecteur qui peut etre integrer dans une boite de dialogue
+		//   le constructeur par default l'instancie dans le repertoire 'Documents'
+		
+		//   Si on veut l'u=instancier on peut utiliser le contructeur prenant une chaine de caracterere
+		//   qui sera le chemin au repertoire souhaité
+		
+		//   On peut utiliser une reference de type File en parametre, qui sera le repertoire dans 
+		//   lequel s'ouvrira le selecteur de fichier
+		
+		// JFileChooser(String directoryPath) utilise le chemin donne
 	    JFileChooser jfc = new JFileChooser( 
 	            FileSystemView
 	            .getFileSystemView()
-	            .getHomeDirectory()
+	            .getHomeDirectory()    // Ouvre le selecteur dans le repertoire Deskstop
 	        );
 
+	    //String s1 = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+	    //System.out.println("test" + s1);
+	    
 	    // l'utilisateur verra seulement les images TIFF
-	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
-	            "Tiff Images", "tif", "tiff");
-	    jfc.setFileFilter(filter);
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter("Tiff Images", "tif", "tiff");
+	    jfc.setFileFilter(filter);  /// assigne le filtre au JFileChooser
+	    
 	    // pas de filtre
 	    //jfc.setAcceptAllFileFilterUsed(false);
 
@@ -92,8 +107,15 @@ public class Process_Pixels implements PlugIn {
 	    jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
 
 	    // Pops up an "Open File" file chooser dialog
+	    //   public int showOpenDialog(Component parent)
+	    //    -> le parametre parent determine le frame dont depend la boite de dialogue depend
+	    //    ->  en cas de parametre parent null le dialogue ne depend pas d'une fenetre visible
+	    
+	    
 	    if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 	    	System.out.println("Source approuvee!");
+	    	
+	    	
 			File selectedInput = jfc.getSelectedFile();
 			input = selectedInput.getPath();
 		     System.out.println("You chose to open this file: " + input); // jfc.getSelectedFile().getName());
@@ -122,11 +144,10 @@ public class Process_Pixels implements PlugIn {
 		LOGGER.info("run() FIN run Autocrop");
 	}
 
-	
-	
-	
+
 	// Verification/enregistrement des fichiers I/O
     // Recuperation des parametres de traitement
+	
 	private void validate() {
 
 			//String input  = autocropDialog.getInput();  // input file
@@ -189,138 +210,7 @@ public class Process_Pixels implements PlugIn {
 	
 	
 	
-	/*
 	
-	//@Override
-	public int setup(String arg, ImagePlus imp) {
-		if (arg.equals("about")) {
-			showAbout();
-			return DONE;
-		}
-
-		image = imp;
-		return DOES_8G | DOES_16 | DOES_32 | DOES_RGB;
-	}
-
-	@Override
-	public void run(ImageProcessor ip) {
-		// get width and height
-		width = ip.getWidth();
-		height = ip.getHeight();
-
-		if (showDialog()) {
-			process(ip);
-			image.updateAndDraw();
-		}
-	}
-
-	private boolean showDialog() {
-		GenericDialog gd = new GenericDialog("Process pixels");
-
-		// default value is 0.00, 2 digits right of the decimal point
-		gd.addNumericField("value", 0.00, 2);
-		gd.addStringField("name", "John");
-
-		gd.showDialog();
-		if (gd.wasCanceled())
-			return false;
-
-		// get entered values
-		value = gd.getNextNumber();
-		name = gd.getNextString();
-
-		return true;
-	}
-    */
-	
-	/*
-	//
-	// Process an image.
-	// <p>
-	// Please provide this method even if {@link ij.plugin.filter.PlugInFilter} does require it;
-	// the method {@link ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)} can only
-	// handle 2-dimensional data.
-	// </p>
-	// <p>
-	// If your plugin does not change the pixels in-place, make this method return the results and
-	// change the {@link #setup(java.lang.String, ij.ImagePlus)} method to return also the
-	// <i>DOES_NOTHING</i> flag.
-	// </p>
-	//
-	// @param image the image (possible multi-dimensional)
-	//
-	public void process(ImagePlus image) {
-		// slice numbers start with 1 for historical reasons
-		for (int i = 1; i <= image.getStackSize(); i++)
-			process(image.getStack().getProcessor(i));
-	}
-
-	// Select processing method depending on image type
-	public void process(ImageProcessor ip) {
-		int type = image.getType();
-		if (type == ImagePlus.GRAY8)
-			process( (byte[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY16)
-			process( (short[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY32)
-			process( (float[]) ip.getPixels() );
-		else if (type == ImagePlus.COLOR_RGB)
-			process( (int[]) ip.getPixels() );
-		else {
-			throw new RuntimeException("not supported");
-		}
-	}
-
-	// processing of GRAY8 images
-	public void process(byte[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (byte)value;
-			}
-		}
-	}
-
-	// processing of GRAY16 images
-	public void process(short[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (short)value;
-			}
-		}
-	}
-
-	// processing of GRAY32 images
-	public void process(float[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (float)value;
-			}
-		}
-	}
-
-	// processing of COLOR_RGB images
-	public void process(int[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (int)value;
-			}
-		}
-	}
-
-	public void showAbout() {
-		IJ.showMessage("ProcessPixels",
-			"a template for processing each pixel of an image"
-		);
-	}
-  */
 	/**
 	 * Main method for debugging.
 	 *
